c32a9d04a36c33e8031b1143c01d9a24db0d8f97
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-07 16:51:53 +0200

    [UPD] Function created

--- PariOdispari/js/script.js
+++ PariOdispari/js/script.js
@@ -6,7 +6,19 @@ console.log("Lo script funziona correttamente")
 //REGISTRIAMO LE SCELTE DEL UTENTE
 let sceltaUser = prompt("Scrivi pari o dispari");
 let sceltaNumUser = parseInt(prompt("scrivi un numero da 1 a 5"));
+let sceltaNumComputer = computerRandomNumber(1, 5);
+let sommaNum = sceltaNumUser + sceltaNumComputer;
+
+//
+console.log(`La scelta del computer è ${sceltaNumComputer} somma: ${sommaNum}`)
 
 //LOGGHIAMO LE SCELTE DEL UTENTE
 console.log(`Hai scelto ${sceltaUser} e il numero ${sceltaNumUser}`)
+
+//FUNZIONE PER GENERARE NUMERO PC
+function computerRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
