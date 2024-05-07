a8cd22fd8b93d258eb4b2dfc01e172ce48af156b
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-07 16:15:24 +0200

    [UPD] We record and log the variables chosen by the user

--- PariOdispari/js/script.js
+++ PariOdispari/js/script.js
@@ -3,3 +3,10 @@
 
 console.log("Lo script funziona correttamente")
 
+//REGISTRIAMO LE SCELTE DEL UTENTE
+let sceltaUser = prompt("Scrivi pari o dispari");
+let sceltaNumUser = parseInt(prompt("scrivi un numero da 1 a 5"));
+
+//LOGGHIAMO LE SCELTE DEL UTENTE
+console.log(`Hai scelto ${sceltaUser} e il numero ${sceltaNumUser}`)
+
