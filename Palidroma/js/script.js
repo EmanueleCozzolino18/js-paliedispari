f1334796af26fe5f126db714e81c0836c01e56c5
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-07 15:53:20 +0200

    [UPD] We record the user's word

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -4,4 +4,12 @@
 //LOG PER VERIFCA CONESSIONE INDEX
 console.log("Lo script è parito correttamente");
 
+//Registriamo la parola del utente
+let paliUser = prompt("Inserisci una parola");
+let paliCheck = (paliUser)
+
+console.log(paliUser)
+console.log(paliCheck)
+
+
 
